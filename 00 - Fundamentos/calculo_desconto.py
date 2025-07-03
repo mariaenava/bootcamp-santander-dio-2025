a919cc72af1c5ade7ff2416b99d014c58a137067
Maria Eduarda Nava <127611550+mariaenava@users.noreply.github.com>
2025-07-03 11:37:29 -0300

    Update calculo_desconto.py

--- 00 - Fundamentos/calculo_desconto.py
+++ 00 - Fundamentos/calculo_desconto.py
@@ -1,20 +1,22 @@
-# Dicionário com os valores de desconto
 descontos = {
     "DESCONTO10": 0.10,
     "DESCONTO20": 0.20,
     "SEM_DESCONTO": 0.00
 }
 
-# Entrada do usuário
-preco = float(input("Preço: ").strip())
+def main():
+    resultado = aplica_desconto(float(input("Preço: ").strip()))
+    print(f"Preço final: R${resultado:.2f}")
 
-# Aplique o desconto se o cupom for válido:
-while True:
-    cupom = input("Cupom de Desconto: ").strip().upper()
-    if cupom in descontos:
-        desconto = preco * descontos[cupom]
-        valor_final = preco - desconto
-        print(f"{valor_final:.2f}")
-        break
-    else:
-        print("Cupom inválido! Tente novamente.")
+
+def aplica_desconto(preco):
+    while True:
+        cupom = input("Cupom de Desconto: ").strip().upper()
+        if cupom in descontos:
+            desconto = preco * descontos[cupom]
+            valor_final = preco - desconto
+            return valor_final
+        else:
+            print("Cupom inválido! Tente novamente.")
+
+main()
